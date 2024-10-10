5e9fadae896e9848d718f5933dbe14625fec41f7
TothBalazs06 <100351575+TothBalazs06@users.noreply.github.com>
2024-10-10 11:18:56 +0200

    a nyul osztaly tulajdonsagai

--- Entities/Rabbit.cs
+++ Entities/Rabbit.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entities
+﻿namespace EntitesLib
 {
     public class Rabbit
     {
+        public int Age { get; private set; }
+        public bool IsAlive { get; private set; }
+        private const int MaxAge = 5; // A nyúl maximum kora
+        private int energy;
+
+        public Rabbit()
+        {
+            energy = 10; // Alap energia szint
+            Age = 0;
+            IsAlive = true; // Él a nyúl
+        }
+
     }
 }
