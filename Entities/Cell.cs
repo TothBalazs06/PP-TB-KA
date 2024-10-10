b5e39a997f7f169af213f87db72e9d0327c0c5ae
KovacsAdamAttila <100351663+KovacsAdamAttila@users.noreply.github.com>
2024-10-10 11:40:05 +0200

    Mező osztály létrehozása
    Mező osztály, későbbiekben ezt fogjuk használni hogy a rácsmezőkben megváltoztassuk a füvet

--- Entities/Cell.cs
+++ Entities/Cell.cs
@@ -1,18 +1,27 @@
-﻿using EntitesLib;
+﻿using System;
 
-namespace Entities
+namespace EntitesLib
 {
     public class Cell
     {
-        public GrassState Grass { get; set; }
-        public Rabbit Rabbit { get; set; }
-        public Fox Fox { get; set; }
-
-        public Cell()
+        public void UpdateGrass()
         {
-            Grass = GrassState.Young; // Alapállapota a fűnek
-            Rabbit = null; // Alapállapota a nyúlnak. vagyis nincs
-            Fox = null; // Alapállapota a rókának, vagyis nincs
+			if (Grass == GrassState.Empty)
+            {
+                Grass = GrassState.Young; // young fű lesz ha empty volt
+            }
+            if (Grass == GrassState.Young)
+            {
+                Grass = GrassState.Mature; // mature fű lesz ha young volt
+            }
+            else if (Grass == GrassState.Mature)
+            {
+                Grass = GrassState.Old; // old lesz ha mature volt
+            }
+            else if (Grass == GrassState.Old)
+            {
+                Grass = GrassState.Empty; // visszaall uresre
+            }
         }
     }
 }
