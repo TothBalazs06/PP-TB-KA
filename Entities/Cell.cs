4f00a79165ec816ee3a674ae573633c6d76d6adf
TothBalazs06 <tothbalazs1902@gmail.com>
2024-10-10 11:32:38 +0200

    cell osztaly tulajdonsagai

--- Entities/Cell.cs
+++ Entities/Cell.cs
@@ -1,7 +1,18 @@
-﻿namespace Entities
+﻿using EntitesLib;
+
+namespace Entities
 {
     public class Cell
     {
+        public GrassState Grass { get; set; }
+        public Rabbit Rabbit { get; set; }
+        public Fox Fox { get; set; }
 
+        public Cell()
+        {
+            Grass = GrassState.Young; // Alapállapota a fűnek
+            Rabbit = null; // Alapállapota a nyúlnak. vagyis nincs
+            Fox = null; // Alapállapota a rókának, vagyis nincs
+        }
     }
 }
