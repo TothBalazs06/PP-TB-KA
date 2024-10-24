34bbf8479d20954b1d580c2b7d3b8422f0fd79e6
KovacsAdamAttila <100351663+KovacsAdamAttila@users.noreply.github.com>
2024-10-24 11:05:50 +0200

    Nyulak unit tesztek

---
+++ TestLib/RabbitTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntitesLib;
+
+namespace TestLib
+{
+    [TestClass]
+    public class RabbitTests
+    {
+        [TestMethod]
+        public void Eat_GrassIncreasesEnergy()
+        {
+            var rabbit = new Rabbit();
+            int initialEnergy = rabbit.Energy;
+
+            rabbit.Eat(GrassState.Young);
+
+            Assert.IsTrue(rabbit.Energy > initialEnergy, "Ha a nyúl megevett egy füvet akkor az energia szintnek nőnie kell.");
+        }
+
+        [TestMethod]
+        public void Survive_ReturnsFalseWhenEnergyIsLow()
+        {
+            var rabbit = new Rabbit();
+
+            for (int i = 0; i < 11; i++) rabbit.Survive();
+
+            Assert.IsFalse(rabbit.IsAlive, "A nyúlnak halottnak kéne lennie ha az energia kisebb mint a megadott.");
+        }
+    }
+}
